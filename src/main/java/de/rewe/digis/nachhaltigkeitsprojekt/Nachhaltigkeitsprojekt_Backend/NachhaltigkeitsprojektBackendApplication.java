41151c42ddf91047b694fcf84e6f128e3286dec6
Philip Neuhäuser <philip.neuhaeuser@rewe-group.com>
2024-03-21 09:50:46 +0100

    Initial Commit

---
+++ src/main/java/de/rewe/digis/nachhaltigkeitsprojekt/Nachhaltigkeitsprojekt_Backend/NachhaltigkeitsprojektBackendApplication.java
@@ -0,0 +1,13 @@
+package de.rewe.digis.nachhaltigkeitsprojekt.Nachhaltigkeitsprojekt_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NachhaltigkeitsprojektBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NachhaltigkeitsprojektBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/rewe/digis/nachhaltigkeitsprojekt/Nachhaltigkeitsprojekt_Backend/NachhaltigkeitsprojektBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rewe.digis.nachhaltigkeitsprojekt.Nachhaltigkeitsprojekt_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachhaltigkeitsprojektBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
