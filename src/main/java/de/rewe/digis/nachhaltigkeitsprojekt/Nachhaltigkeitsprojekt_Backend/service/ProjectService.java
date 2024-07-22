4a378fff4a1d1b9ba89af7c891b5f1df36f7109a
Philip Neuhäuser <philip.neuhaeuser@rewe-group.com>
2024-07-22 15:42:57 +0200

    Reformat ProjectService

--- src/main/java/de/rewe/digis/nachhaltigkeitsprojekt/Nachhaltigkeitsprojekt_Backend/service/ProjectService.java
+++ src/main/java/de/rewe/digis/nachhaltigkeitsprojekt/Nachhaltigkeitsprojekt_Backend/service/ProjectService.java
@@ -22,28 +22,54 @@ public class ProjectService {
 
     public Project getProjectById(long id) {
         Set<Project> projects = ProjectService.importProjectsFromJSON();
-        Optional<Project> foundProject = projects.stream().filter(project -> project.getId() == id).findFirst();
+        Optional<Project> foundProject = projects
+                .stream()
+                .filter(project -> project.getId() == id)
+                .findFirst();
         return foundProject.orElse(null);
     }
 
     public Set<Project> getProjectsByTitle(String title) {
         Set<Project> projects = ProjectService.importProjectsFromJSON();
-        return projects.stream().filter(project -> project.getTitle().toLowerCase().contains(title.toLowerCase())).collect(Collectors.toSet());
+        return projects
+                .stream()
+                .filter(project -> project
+                        .getTitle()
+                        .toLowerCase()
+                        .contains(title.toLowerCase()))
+                .collect(Collectors.toSet());
     }
 
     public Set<Project> getProjectsByDescription(String description) {
         Set<Project> projects = ProjectService.importProjectsFromJSON();
-        return projects.stream().filter(project -> project.getDescription().toLowerCase().contains(description.toLowerCase())).collect(Collectors.toSet());
+        return projects
+                .stream()
+                .filter(project -> project
+                        .getDescription()
+                        .toLowerCase()
+                        .contains(description.toLowerCase()))
+                .collect(Collectors.toSet());
     }
 
     public Set<Project> getProjectsByOwner(String owner) {
         Set<Project> projects = ProjectService.importProjectsFromJSON();
-        return projects.stream().filter(project -> project.getOwner().toLowerCase().contains(owner.toLowerCase())).collect(Collectors.toSet());
+        return projects
+                .stream()
+                .filter(project -> project
+                        .getOwner()
+                        .toLowerCase()
+                        .contains(owner.toLowerCase()))
+                .collect(Collectors.toSet());
     }
 
     public Set<Project> getProjectsByTags(Set<Tags> tags) {
         Set<Project> projects = ProjectService.importProjectsFromJSON();
-        return projects.stream().filter(project -> project.getTags().containsAll(tags)).collect(Collectors.toSet());
+        return projects
+                .stream()
+                .filter(project -> project
+                        .getTags()
+                        .containsAll(tags))
+                .collect(Collectors.toSet());
     }
 
     private static Set<Project> importProjectsFromJSON() {
@@ -53,7 +79,8 @@ public class ProjectService {
         Set<Project> projects = new HashSet<>();
 
         try {
-            projects = objectMapper.readValue(new File(jsonPath), new TypeReference<HashSet<Project>>() {});
+            projects = objectMapper.readValue(new File(jsonPath), new TypeReference<HashSet<Project>>() {
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
